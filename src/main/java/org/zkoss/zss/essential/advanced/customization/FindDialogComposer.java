7ddac0469926e792ba890d6065e251dac89a9a49
hawk <hawkchen@potix.com>
2016-03-24 16:35:25 +0800

    find dialog - add "search scope" in a find dialog

--- src/main/java/org/zkoss/zss/essential/advanced/customization/FindDialogComposer.java
+++ src/main/java/org/zkoss/zss/essential/advanced/customization/FindDialogComposer.java
@@ -1,16 +1,21 @@
 package org.zkoss.zss.essential.advanced.customization;
 
+import java.awt.geom.Area;
+
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.MouseEvent;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zss.api.AreaRef;
 import org.zkoss.zss.api.Range;
 import org.zkoss.zss.api.Ranges;
+import org.zkoss.zss.api.model.Book;
 import org.zkoss.zss.api.model.CellData.CellType;
 import org.zkoss.zss.api.model.Sheet;
 import org.zkoss.zss.ui.Spreadsheet;
+import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
@@ -21,28 +26,73 @@ import org.zkoss.zul.Window;
 @SuppressWarnings("serial")
 public class FindDialogComposer extends SelectorComposer<Component> {
 	@Wire
-	private Window findDialog;
+	protected Window findDialog;
 	@Wire
-	private Textbox keywordBox;
+	protected Textbox keywordBox;
+	@Wire
+	protected Combobox scopeBox;
+	
+	//TODO fill the combobox with a model
+	protected String[] scope = {"sheet", "book"};
 	
 	@Listen("onClick=#find; onOK=#keywordBox")
 	public void find(Event event) {
 		Spreadsheet ss = (Spreadsheet)findDialog.getAttribute(Spreadsheet.class.toString());
-		Sheet currentSheet = ss.getSelectedSheet();
-		int lastColumn = Ranges.range(currentSheet).getDataRegion().getLastColumn();
-		int lastRow = Ranges.range(currentSheet).getDataRegion().getLastRow();
+		Range targetCell = null;
+		if (scopeBox.getSelectedItem().getValue().equals(scope[0])){
+			targetCell = findNext(ss.getSelectedSheet(), ss.getSelection());
+		}else{
+			targetCell = findNext(ss.getBook(), ss.getSelectedSheet(), ss.getSelection());
+		}
+		jumpToCell(ss, targetCell);
+	}
+
+
+	protected void jumpToCell(Spreadsheet ss, Range targetCell) {
+		ss.setSelectedSheet(targetCell.getSheetName());
+		ss.focusTo(targetCell.getRow(), targetCell.getColumn());
+	}
+	
+	protected Range findNext(Book book, Sheet startingSheet, AreaRef stargingSelection) {
+		Sheet sheet = startingSheet;
+		AreaRef selection = stargingSelection;
+		Range foundCell = null;
+		int index = book.getSheetIndex(sheet);
+		while (index < book.getNumberOfSheets()){
+			sheet = book.getSheetAt(index);
+			foundCell = findNext(sheet, selection);
+			if (selection.getRow() == foundCell.getRow() &&
+				selection.getColumn() == foundCell.getColumn()){ //nothing matched, move to next sheet
+				index++;
+				selection = new AreaRef(0, 0, 0, 0);
+			}else{
+				return foundCell;
+			}
+		}
+		
+		return foundCell;
+	}
+	
+	/**
+	 * If nothing found, return current selected cell.
+	 * @param sheet
+	 * @param selection
+	 * @return
+	 */
+	protected Range findNext(Sheet sheet, AreaRef selection) {
+		int lastColumn = Ranges.range(sheet).getDataRegion().getLastColumn();
+		int lastRow = Ranges.range(sheet).getDataRegion().getLastRow();
 		String keyword = keywordBox.getValue().trim();
 		//starting from current selection position
-		int row = ss.getSelection().getRow();
-		int column = ss.getSelection().getColumn()+1; 
+		int row = selection.getRow();
+		int column = selection.getColumn()+1; 
 		// by rows
 		while (row <= lastRow){
 			while (column <= lastColumn){
-				Range cell = Ranges.range(currentSheet, row, column);
+				Range cell = Ranges.range(sheet, row, column);
 				if (cell.getCellData().getType() == CellType.STRING){
 					if (cell.getCellData().getEditText().toLowerCase().contains(keyword)){
-						ss.focusTo(row, column);
-						return;
+						return cell;
 					}
 				}
 				column++;
@@ -50,8 +100,9 @@ public class FindDialogComposer extends SelectorComposer<Component> {
 			column = 0;
 			row++;
 		}
+		return Ranges.range(sheet, selection);
 	}
-	
+
 	@Listen("onClick = #close")
 	public void close(MouseEvent event) {
 		findDialog.setVisible(false);
