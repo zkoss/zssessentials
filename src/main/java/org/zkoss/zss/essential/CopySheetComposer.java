8a79ee0541b15c7537a607a2a0c591e7441ba545
hawkchen <hawkchen@potix.com>
2020-02-05 17:18:14 +0800

    apply clone sheet wokraround

--- src/main/java/org/zkoss/zss/essential/CopySheetComposer.java
+++ src/main/java/org/zkoss/zss/essential/CopySheetComposer.java
@@ -4,13 +4,14 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
 import org.zkoss.zss.api.model.Book;
-import org.zkoss.zss.essential.util.BookUtil;
+import org.zkoss.zss.essential.util.*;
+import org.zkoss.zss.model.SName;
 import org.zkoss.zss.ui.Spreadsheet;
+import org.zkoss.zss.ui.event.Events;
 
 /**
  * @author Hawk
  */
-@SuppressWarnings("serial")
 public class CopySheetComposer extends SelectorComposer<Component> {
 
 	@Wire
@@ -18,10 +19,21 @@ public class CopySheetComposer extends SelectorComposer<Component> {
 	@Wire
 	private Spreadsheet targetSpreadsheet;
 	
-	@Listen("onClick = #copy")
+	@Listen("onClick = #copy2new")
 	public void copySheetToNewBook(){
 		Book newBook = BookUtil.copySheetToNewBook("newOne", srcSpreadsheet.getSelectedSheet());
 		targetSpreadsheet.setBook(newBook);
 	}
-	
+
+	@Listen("onClick = #copy")
+	public void copySheet(){
+		SheetCopier.clone("cloned", srcSpreadsheet.getSelectedSheet());
+	}
+
+	@Listen(Events.ON_CELL_CLICK + " = #list")
+	public void listNames(){
+		for (SName n : srcSpreadsheet.getBook().getInternalBook().getNames()) {
+			System.out.printf("%s %s %s\n", n.getName() , n.getApplyToSheetName(), n.getRefersToFormula());
+		}
+	}
 }
